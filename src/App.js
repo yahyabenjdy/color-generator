bfccb4fdff837a60e6ebb0c44c10ce5f0b09dd1d
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2023-03-06 21:49:50 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react";
 import SingleColor from "./SingleColor";
 
-// import Values from "values.js";
+import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
     <>
       <section className="container">
         <h3>color generator</h3>
-        <form onSubmit={handleSubmit}></form>
-        <input
-          type="text"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-          placeholder="f15025"
-        />
-        <button className="btn" type="submit">
-          submit
-        </button>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
